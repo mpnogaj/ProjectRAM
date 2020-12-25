a4a3260808464a0d8e73df8547ffa2143d76efae
Marcin Nogaj <mpnogaj@gmail.com>
2020-12-25 13:28:44 +0100

    Deleted docker, added api controller

---
+++ RAMWebsite/Controllers/ApiController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using RAMWebsite.Models;
+
+namespace RAMWebsite.Controllers
+{ 
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        [Route("api/test")]
+        public string test()
+        {
+            return JsonConvert.SerializeObject(new Test { Id = "15" });
+        }
+    }
+}
