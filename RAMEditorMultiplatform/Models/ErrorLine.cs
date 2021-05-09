1211a93be079b5369abe50a5953ddb29d3677219
mpnogaj <mpnogaj@gmail.com>
2021-05-09 14:16:54 +0200

    Added ErrorLine.cs model and ui for program validation

---
+++ RAMEditorMultiplatform/Models/ErrorLine.cs
@@ -0,0 +1,8 @@
+﻿namespace RAMEditorMultiplatform.Models
+{
+    public class ErrorLine
+    {
+        public string Message { get; set; } = string.Empty;
+        public long Line { get; set; } = -1;
+    }
+}
